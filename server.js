d4a4d1f341d7e7c6486b5d4aa60b7624b712fd28
Benjamin Schoolland <bschoolland@gmail.com>
2024-08-05 22:32:14 -0700

    feat: switch to port 3004

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const db = require("./db");
 const app = express();
-const port = 3000;
+const port = 3004;
 
 app.use(express.json());
 
